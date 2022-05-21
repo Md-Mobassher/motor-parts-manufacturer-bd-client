91cabbfdcc1e111c8527241412137ebc81834fde
Md-Mobassher <you@example.com>
2022-05-22 01:50:22 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Login/Login';
+import Signup from './Login/Signup';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Navbar from './Shared/Navbar';
+import NotFound from './Shared/NotFound';
 
 function App() {
   return (
-    <div>
-      <button class="btn btn-primary">Button</button>
+    <div className='max-w-7xl mx-auto lg:px-12'>
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </Navbar>
+      
     </div>
   );
 }
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {  NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({children}) => {
+    let navItems = <>
+        <li><NavLink to='/'  className='rounded-lg'>Home</NavLink></li>
+        <li><NavLink to='/purchase'  className='rounded-lg'>Purchase</NavLink></li>
+        <li><NavLink to='/dashboard' className='rounded-lg'>Dashboard</NavLink></li>
+        <li><NavLink to='/blogs' className='rounded-lg'>Blogs</NavLink></li>
+        <li><NavLink to='/login' className='rounded-lg'>Login</NavLink></li>
+        
+    </>
+
+
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className="drawer drawer-end">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+        <div className="drawer-content flex flex-col">
+ 
+          <div className="w-full navbar ">
+          <div className="flex-1 px-2 mx-2 text-2xl font-semibold ">Motor Parts BD</div>
+            <div className="flex-none lg:hidden">
+              <label for="my-drawer-3" className="btn btn-square btn-ghost ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              </label>
+            </div> 
+            
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal ">
+                    {navItems}
+              </ul>
+            </div>
+          </div>
+
+       {children}
+        </div> 
+        <div className="drawer-side">
+          <label for="my-drawer-3" className="drawer-overlay"></label> 
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+                {navItems}
+          </ul>
+          
         </div>
+      </div>
     );
 };
 
