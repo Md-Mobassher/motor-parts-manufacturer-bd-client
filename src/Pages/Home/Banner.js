82da0869080df45e07057725f5826526f30b5396
Md-Mobassher <you@example.com>
2022-06-23 01:30:06 +0600

    banner updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,42 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import bike1 from '../../Assets/image/banner/bike1.jpg'
-import bike2 from '../../Assets/image/banner/bike2.jpg'
-import bike3 from '../../Assets/image/banner/bike3.jpg'
-import bike4 from '../../Assets/image/banner/bike5.jpg'
+
 
 const Banner = () => {
     return (
         <section>
-            <div className="carousel w-screen h-screen">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src={bike1} className="w-full" alt='bike' />  
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a> 
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+            <div class="hero min-h-screen">
+                <div class="hero-content flex-col lg:flex-row-reverse lg:px-20 lg:-mt-10">
+                    <div className='flex-1'>
+                        <img src={bike1} class=" rounded-lg  " alt=''/>
                     </div>
-                </div> 
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src={bike2} className="w-full" alt=''  /> 
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a> 
-                    <a href="#slide3" className="btn btn-circle">❯</a>
+                    <div className='flex-1 p-5'>
+                        <h1 class="lg:text-5xl text-4xl font-bold mb-3">Best Motor Parts</h1>
+                        <p class="py-6 mb-3">We are the motor parts manufacturer company. We provide the best quality of motor parts. Buy our products you will get life time membership from us.</p>
+                        <Link to='' class="btn btn-primary">Buy Now</Link>
                     </div>
-                </div> 
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src={bike3} className="w-full" alt='' />  
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a> 
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div> 
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src={bike4} className="w-full" alt='' />  
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a> 
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div> 
-                
+                    
+                </div>
             </div>
         </section>
     );
